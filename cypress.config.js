b720cbdf6ceab074182c00b31853bcdfaca06d0d
AMAIISDOM\blamkhatri <btissame.lamkhatri@amadeus.com>
2024-06-06 18:07:47 +0200

    test

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
       // implement node event listeners here
       on('file:preprocessor', cucumber())
     },
-    specPattern: "cypress/e2e/1-getting-started/*.cy.js",
+    specPattern: "cypress/e2e/*.feature",
   },
 
 });
